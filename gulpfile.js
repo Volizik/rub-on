7875a535ff3e0f3b24160fc7f77b25e271d14fd2
Kizilov <kizilov.yura@gmail.com>
2017-10-30 17:57:33 +0300

    ///

--- gulpfile.js
+++ gulpfile.js
@@ -4,49 +4,31 @@ var gulp = require('gulp'), // Подключаем Gulp
     concat = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
     uglify = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
     rename = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    del = require('del'), // Подключаем библиотеку для удаления файлов и папок
     imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
     pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    spritesmith = require('gulp.spritesmith'), // Подключаем библиотеку для создания png-спрайтов
+    svgstore = require('gulp-svgstore'),//// Подключаем библиотеку для объединения SVG в один файл
+    svgmin = require('gulp-svgmin'),//Подключаем библиотеку для минификации SVG
     cache = require('gulp-cache'), // Подключаем библиотеку кеширования
     extender = require('gulp-html-extend'),//Подключаем бибилиотеку для склейки html-файлов
     sourcemaps = require('gulp-sourcemaps'),//Подключаем плагин, записывающий карту источника в исходный файл
     rimraf = require('rimraf'),//Очищает указанные исходники
-    plumber = require('gulp-plumber'),//Подключаем плагин, который не останавливает задачи от остановки во время их выполнения при возникновении ошибки
-    postcss = require('gulp-postcss'),//Блиотека-парсер стилей для работы с postcss-плагинами
+    plumber = require('gulp-plumber');//Подключаем плагин, который не останавливает задачи от остановки во время их выполнения при возникновении ошибки
+
+var postcss = require('gulp-postcss'),//Блиотека-парсер стилей для работы с postcss-плагинами
     autoprefixer = require('autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
     cssnano = require('cssnano'),//postcss-плагин, для минификации CSS кода, идущего на продакшен.
+    pxtorem = require('postcss-pxtorem'),//postcss-плагин, для перевода px в rem, идущего на продакшен.
+    pxtoem = require('postcss-px-to-em'),//postcss-плагин, для перевода px в em, идущего на продакшен.
     short = require('postcss-short'),
     stylefmt = require('stylefmt'),
     assets = require('postcss-assets'),
+    shortspacing = require('postcss-short-spacing'),
+    focus = require('postcss-focus'),//postcss-плагин, делающие стили :hover и :focus одинаковыми.
     sorting = require('postcss-sorting'),
     fontmagic = require('postcss-font-magician'),
     fixes = require('postcss-fixes');
-
-
-gulp.task("clean", function (cb) {
-    rimraf('./js/script.min.js', cb);
-});
-
-gulp.task('compress', ['clean'], function () {// Создаем таск compress
-    return gulp.src('app/js/*.js')// Берем все необходимые библиотеки
-        .pipe(plumber())
-        .pipe(concat('script.js'))// Собираем их в кучу в новом файле script.js
-        .pipe(rename({
-            suffix: ".min",// Добавляем суффикс .min
-            extname: ".js"// Добавляем окончание .js
-        }))
-        .pipe(uglify()) // Сжимаем JS файл
-        .pipe(plumber.stop())
-        .pipe(gulp.dest('js'))// Выгружаем в папку js
-        .pipe(browserSync.stream({}));
-
-});
-
-gulp.task('extend-pages', function () {
-    gulp.src('./app/html/pages/*.html')
-        .pipe(extender({annotations: false, verbose: false})) // default options
-        .pipe(gulp.dest('./'))
-        .pipe(browserSync.stream({}));
-});
 
 gulp.task('css-libs', function () { // Создаем таск css-libs
     var processors = [
@@ -61,21 +43,6 @@ gulp.task('css-libs', function () { // Создаем таск css-libs
         .pipe(browserSync.stream({})); // Обновляем CSS на странице при изменении
 });
 
-gulp.task('img', function () {
-    return gulp.src('app/img/**/*')
-        .pipe(cache(imagemin({
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{
-                removeViewBox: false
-            }],
-            use: [pngquant()]
-        })))
-        .pipe(gulp.dest('images'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
 
 gulp.task('sass', function () { // Создаем таск Sass
     var processors = [// подключаем постпроцессоры в массиве
@@ -119,12 +86,62 @@ gulp.task('browser-sync', function () { // Создаем таск browser-sync
     });
 });
 
+gulp.task('compress', ['clean'], function () {// Создаем таск compress
+    return gulp.src('app/js/*.js')// Берем все необходимые библиотеки
+        .pipe(plumber())
+        .pipe(concat('script.js'))// Собираем их в кучу в новом файле script.js
+        .pipe(rename({
+            suffix: ".min",// Добавляем суффикс .min
+            extname: ".js"// Добавляем окончание .js
+        }))
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('js'))// Выгружаем в папку js
+        .pipe(browserSync.stream({}));
+
+});
+
+gulp.task("clean", function (cb) {
+    rimraf('./js/script.min.js', cb);
+});
+
+gulp.task('extend-pages', function () {
+    gulp.src('./app/html/pages/*.html')
+        .pipe(extender({annotations: false, verbose: false})) // default options
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream({}));
+});
+
+gulp.task('extend-blocks', function () {
+    gulp.src('./app/html/*.html')
+        .pipe(extender({annotations: true, verbose: false})) // default options
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream({}));
+});
+
 gulp.task('watch', ['compress', 'extend-pages', 'css-libs', 'img', 'sass'], function () {
     gulp.watch('app/libs/**/*', ['css-libs']); // Наблюдение за папкой libs
     gulp.watch('app/images/**/*', ['img']);// Наблюдение за папкой images
     gulp.watch('app/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
     gulp.watch(['app/html/**/*.html'], ['extend-pages']);// Наблюдение за HTML-файлами в папке html
     gulp.watch('app/js/**/*.js', ['compress']); // Наблюдение за js-файлами
+});
+
+
+gulp.task('img', function () {
+    return gulp.src('app/img/**/*')
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('images'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('clear', function (callback) {
@@ -134,4 +151,4 @@ gulp.task('clear', function (callback) {
 gulp.task('default', ['watch', 'browser-sync']);
 
 /*
- npm i gulp gulp-sass browser-sync gulp-concat gulp-uglifyjs gulp-rename gulp-imagemin imagemin-pngquant gulp-cache gulp-html-extend gulp-sourcemaps rimraf gulp-plumber gulp-postcss autoprefixer cssnano postcss-pxtorem postcss-px-to-em postcss-short stylefmt postcss-assets postcss-short-spacing postcss-focus postcss-sorting postcss-font-magician postcss-fixes stylelint-config-standard --save-dev*/
+ npm i gulp gulp-sass browser-sync gulp-concat gulp-uglifyjs gulp-rename del gulp-imagemin imagemin-pngquant calipers-png calipers-jpeg calipers-gif gulp.spritesmith gulp-svgstore gulp-svgmin gulp-cache gulp-html-extend gulp-sourcemaps rimraf gulp-plumber gulp-postcss autoprefixer cssnano postcss-pxtorem postcss-px-to-em postcss-short stylefmt postcss-assets postcss-short-spacing postcss-focus postcss-sorting postcss-font-magician postcss-fixes stylelint-config-standard --save-dev*/
